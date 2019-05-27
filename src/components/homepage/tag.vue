ef6e0a051b35f11157bd4c8810213b5c2dbcc73d
merciqiao <qiaolixue@yingu.com>
2019-05-27 18:20:33 +0800

    1.标签页增加标签功能按钮

--- src/components/homepage/tag.vue
+++ src/components/homepage/tag.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="_tag">
-    <el-tag
-      v-for="tag in tagsList"
-      :key="tag.title"
-      :closable="!tag.hideclose"
-      :type="isActive(tag)"
-      @close="handleCloseTag(tag)"
-    >
-     <router-link  :to="tag.path" class="tag-title">
-                    {{tag.title}}
-     </router-link>
-    </el-tag>
+    <div class="left">
+      <el-tag
+        v-for="tag in tagsList"
+        :key="tag.title"
+        :closable="!tag.hideclose"
+        :type="isActive(tag)"
+        @close="handleCloseTag(tag)"
+      >
+        <router-link :to="tag.path" class="tag-title">{{tag.title}}</router-link>
+      </el-tag>
+    </div>
+
+    <div class="right">
+      <el-dropdown trigger="click">
+        <el-button type="primary" size="small">
+          更多菜单
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>关闭其它</el-dropdown-item>
+          <el-dropdown-item>关闭所有</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 <style lang="scss">
@@ -20,13 +33,27 @@
   align-items: center;
   background-color: white;
   border-bottom: 1px solid #e6e6e6;
-  .el-tag {
-    margin-left: 6px;
-    .tag-title{
-      color:black;
-      line-height: 32px;
-      padding: 8px 2px;
+  justify-content: space-between;
+  .left {
+    display: flex;
+    flex-flow:row nowrap;
+    .el-tag {
+      margin-left: 6px;
+      .tag-title {
+        color: black;
+        line-height: 32px;
+        padding: 8px 2px;
+      }
     }
+  }
+
+  .right {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.1) -3px 0px 15px 3px;
+    padding: 0 5px;
   }
 }
 </style>
@@ -50,31 +77,29 @@ export default {
       !isExsit &&
         this.tagsList.push({
           title: route.meta.title, //标签名
-          name:route.name,//路由里的name对应vue页的name,标签列表里的name可以做vue页面缓存
+          name: route.name, //路由里的name对应vue页的name,标签列表里的name可以做vue页面缓存
           path: route.fullPath, //路由
           hideclose: route.meta.hideclose ? route.meta.hideclose : false //是否隐藏关闭
         });
     },
     //关闭标签
-    handleCloseTag(tag){
+    handleCloseTag(tag) {
       this.tagsList.splice(this.tagsList.indexOf(tag), 1);
-      if(this.tagsList.length>0){
-        this.$router.push(this.tagsList[this.tagsList.length-1].path);
-      }
-      else{
-        this.$router.push('/index');
+      if (this.tagsList.length > 0) {
+        this.$router.push(this.tagsList[this.tagsList.length - 1].path);
+      } else {
+        this.$router.push("/index");
       }
     },
-    handleClick(tag){
+    handleClick(tag) {
       this.$router.push(tag.fullPath);
     },
     //是否选中
-    isActive(tag){
-      if(tag.path==this.$route.fullPath){
-        return '';
-      }
-      else{
-        return 'info';
+    isActive(tag) {
+      if (tag.path == this.$route.fullPath) {
+        return "";
+      } else {
+        return "info";
       }
     }
   },
