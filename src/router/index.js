19555c6822d9daeb9db26c79688d96e616368132
merciqiao <qiaolixue@yingu.com>
2019-05-29 17:52:34 +0800

    1.添加请求拦截器,请求头加token

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import axios from 'axios'
 
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
   routes: [
     {
       path: '/',
       redirect: '/login'
     },
-     {
-      name:'login',
+    {
+      name: 'login',
       path: '/login',
       component: () => import('@/components/login.vue'),
       meta: { title: '登录页' }
@@ -21,24 +22,24 @@ export default new Router({
       meta: {
         title: '母版页'
       },
-      children:[
+      children: [
         {
           path: '/index',
           name: '系统首页',
           component: () => import('@/components/test/HelloWorld'),
           meta: {
             title: '系统首页',
-            hideclose:true,
-            role:['王者岗','白银岗']
+            hideclose: true,
+            role: ['王者岗', '白银岗']
           }
         },
-         {
+        {
           path: '/test01',
           name: 'test01',
           component: () => import('@/components/test/test01'),
           meta: {
             title: 'test01页',
-            role:['白银岗']
+            role: ['白银岗']
           }
         },
         {
@@ -47,19 +48,19 @@ export default new Router({
           component: () => import('@/components/tablepage/tablepage'),
           meta: {
             title: 'tablepage页',
-            role:['王者岗','白银岗']
+            role: ['王者岗', '白银岗']
           }
         },
       ]
     },
     {
-      name:'404',
+      name: '404',
       path: '/404',
       component: () => import('@/components/homepage/404.vue'),
       meta: { title: '路由不存在' }
     },
     {
-      name:'403',
+      name: '403',
       path: '/403',
       component: () => import('@/components/homepage/403.vue'),
       meta: { title: '资源不可访问' }
@@ -69,5 +70,71 @@ export default new Router({
       redirect: '/404'
     }
   ],
-  mode:'history'
+  mode: 'history'
 })
+/**
+ * 全局路由守卫
+ */
+const rightPathList=['/login','/404','/403'];//直接可以进入的页面
+router.beforeEach((to, from, next) => {
+  // debugger
+  console.log('跳转到:', to.fullPath);
+  var token = sessionStorage.getItem('token');
+  if (!token&&to.path!='/login') {//登陆认证校验,没登录则跳转/login
+      next({ path: '/login' })
+  }
+  else {//权限认证
+    if(rightPathList.includes(to.path)){
+      next();
+    }
+    else if(hasPermit(to)){
+      next();
+    }
+    else{
+      next('403');
+    }
+    
+  }
+})
+/**
+ * 请求拦截器,添加请求头token
+ */
+axios.interceptors.request.use(
+  config => {
+      var headers=config.headers;
+      if(sessionStorage.getItem("token")){
+        headers.token=sessionStorage.getItem("token");
+      }
+      return config;
+  },
+  err => {
+      return Promise.reject(err);
+  });
+
+
+
+//获取当前路由是否有权限访问
+function hasPermit(to){
+  var hasPermit=false;
+  if(to.meta&&to.meta.role){
+    var ruleList= getRuleList();
+    hasPermit=ruleList.some(rule=>{
+      return to.meta.role.includes(rule);
+    });
+  }
+  return hasPermit;
+  
+}
+//获取登陆的角色集合
+function getRuleList() {
+  var ruleList = []; //角色数组
+  var strRule = sessionStorage.getItem("position");
+  if (strRule.indexOf("|") != -1) {
+    ruleList = strRule.split("|");
+  } else {
+    ruleList.push(strRule);
+  }
+  return ruleList;
+}
+
+export default router
